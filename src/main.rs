6f2ff9dccbeafdcb01188f0e27f891828ad1368c
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-11-16 20:57:30 +0100

    cli

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,26 @@
+use clap::{App, Arg, AppSettings};
 
-fn main() {
-    println!("Hello, world!");
+const NAME: &str =env!("CARGO_PKG_NAME");
+const DESCRIPTION: &str =env!("CARGO_PKG_DESCRIPTION");
+const VERSION: &str = env!("CARGO_PKG_VERSION");
+const AUTHORS: &str = env!("CARGO_PKG_AUTHORS");
+
+fn main(){
+    let matches = App::new(NAME)
+        .version(VERSION)
+        .author(AUTHORS)
+        .about(DESCRIPTION)
+        .setting(AppSettings::ArgRequiredElseHelp)
+        .subcommand(App::new("config")
+            .about("Config")
+            .arg(Arg::new("url")
+                 .short('u')
+                 .long("url")
+                 .required(true)
+                 .takes_value(true))
+            .short('c')
+            .long("config")
+            .takes_value(false))
+        .get_matches();
+
 }
